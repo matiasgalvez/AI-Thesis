d55266872c2fc896c756d7580ae5943d414ce27b
Matias Galvez <mattg1593@gmail.com>
2020-06-29 05:02:02 -0300

    Progress

--- keras_first_network.py
+++ keras_first_network.py
@@ -13,14 +13,17 @@ y = dataset[:,8]
 # define the keras model
 model = Sequential()
 model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(8, activation= 'relu'))
+model.add(Dense(6, activation= 'relu'))
+model.add(Dense(4, activation= 'relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 # compile the keras model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # fit the keras model on the dataset
-model.fit(x, y, epochs=150, batch_size=10)
+model.fit(x, y, epochs=150, batch_size=1) #this gets the ACC up to 80%
 
 # evaluate the keras model
 _, accuracy = model.evaluate(x, y)
