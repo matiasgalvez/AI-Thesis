387effd484dec3eb95571f3a029c201fa1ed1086
Matias Galvez <mattg1593@gmail.com>
2020-06-28 21:32:02 -0300

    Update deprecated prediction method

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # AI-Thesis
-# This repo will be where all the source code for my thesis will be 
+## This repo will be where all the source code for my thesis will be 
--- keras_first_network.py
+++ keras_first_network.py
@@ -5,6 +5,7 @@ from keras.layers import Dense
 
 # load the dataset
 dataset = loadtxt('pima-indians-diabetes.csv', delimiter=',')
+
 # split into input (X) and output (y) variables
 x = dataset[:,0:8]
 y = dataset[:,8]
@@ -25,8 +26,9 @@ model.fit(x, y, epochs=150, batch_size=10)
 _, accuracy = model.evaluate(x, y)
 print('Accuracy: %.2f' % (accuracy*100))
 
-# make class predictions with the model
-predictions = model.predict_classes(x)
+# make class predictions with the model (model.predict_classes(x) deprecated version)
+predictions = (model.predict(x) > 0.5).astype("int32")
+
 # summarize the first 5 cases
 for i in range(5):
   print('%s => %d (expected %d)' % (x[i].tolist(), predictions[i], y[i]))
